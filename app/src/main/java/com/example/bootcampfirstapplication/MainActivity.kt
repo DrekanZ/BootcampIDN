9d908f2dd5e723e9819a008767ea4534f4f840be
DrekanZ <drekanz21@gmail.com>
2023-02-07 09:56:42 +0700

    TugasActivity

--- app/src/main/java/com/example/bootcampfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/bootcampfirstapplication/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.bootcampfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // test
+        val btnToTugas1 =findViewById<Button>(R.id.buttonTugas1);
+        btnToTugas1.setOnClickListener {
+            val intent = Intent(this, Tugas1::class.java)
+            startActivity(intent)
+        }
     }
 }
