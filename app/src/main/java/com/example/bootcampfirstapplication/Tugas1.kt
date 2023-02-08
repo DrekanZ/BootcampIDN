ede9cba89cedb7b634835693522dc9d825ed6915
DrekanZ <drekanz21@gmail.com>
2023-02-08 10:43:41 +0700

    Fix String Resource (Paragraph containing apostrophe) and adding text alignment in game description

--- app/src/main/java/com/example/bootcampfirstapplication/Tugas1.kt
+++ app/src/main/java/com/example/bootcampfirstapplication/Tugas1.kt
@@ -1,11 +1,18 @@
 package com.example.bootcampfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Tugas1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tugas1)
+        val btnToMain = findViewById<ImageView>(R.id.backButton)
+        btnToMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
